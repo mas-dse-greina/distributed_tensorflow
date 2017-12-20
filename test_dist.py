0efc822eec3fc7c35de713cb14a80663a5685b85
Tony Reina <greina@eng.ucsd.edu>
2017-12-19 17:11:14 -0800

    Distibuted TensorFlow

--- test_dist.py
+++ test_dist.py
@@ -28,7 +28,7 @@ slope = 5
 intercept = 13
 
 CHECKPOINT_DIRECTORY = './checkpoints/'
-NUM_STEPS = 10000
+NUM_STEPS = 50000
 
 ####################################################################
 
@@ -189,7 +189,7 @@ def main(_):
 
 		if step % steps_to_validate == 0:
 		  w,b = sess.run([weight,bias])
-		  print("step: {}, Predicted Slope: {:.3f} (True slope = {}), Predicted Intercept: {:.3f} (True intercept = {}, loss: {:.4f}".format(step, w[0], slope, b[0], intercept, loss_v[0]))
+		  print("step: {} of {}, Predicted Slope: {:.3f} (True slope = {}), Predicted Intercept: {:.3f} (True intercept = {}, loss: {:.4f}".format(step, NUM_STEPS, w[0], slope, b[0], intercept, loss_v[0]))
 
 	
 	  # Send a signal to the ps when done by simply updating a queue in the shared graph
