709b552768716c7e3b7fac6881c81fe2c3233c61
Tony Reina <greina@eng.ucsd.edu>
2017-12-20 14:15:42 -0800

    sync update

--- test_dist.py
+++ test_dist.py
@@ -200,7 +200,7 @@ def main(_):
 	sv = tf.train.Supervisor(is_chief=is_chief,
 		logdir=CHECKPOINT_DIRECTORY,
 		init_op=init_op,
-		summary_op=summary_op, 
+		summary_op=None, 
 		saver=saver,
 		global_step=global_step,
 		save_model_secs=60)  # Save the model (with weights) everty 60 seconds
