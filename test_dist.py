a179909b9bf130f558d502a1e1f5e867d24edaa7
Tony Reina <greina@eng.ucsd.edu>
2017-12-19 18:01:19 -0800

    Summary op

--- test_dist.py
+++ test_dist.py
@@ -192,8 +192,11 @@ def main(_):
 		# Define a line with random noise
 		train_x = np.random.randn(1)*10
 		train_y = slope * train_x + np.random.randn(1) * 0.33  + intercept
-		
-		_, loss_v, step = sess.run([train_op, loss_value, global_step], feed_dict={input:train_x, label:train_y})
+
+		if (task_index == 0):
+			_, loss_v, step, summary = sess.run([train_op, loss_value, global_step, summary_op], feed_dict={input:train_x, label:train_y})
+		else:
+			_, loss_v, step = sess.run([train_op, loss_value, global_step], feed_dict={input:train_x, label:train_y})
 
 		if step % steps_to_validate == 0:
 		  w,b = sess.run([weight,bias])
