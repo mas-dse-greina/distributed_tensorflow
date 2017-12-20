7a918eefc48d8e16d89c5308569db11713be3929
Tony Reina <greina@eng.ucsd.edu>
2017-12-20 10:24:20 -0800

    sync update

--- test_dist.py
+++ test_dist.py
@@ -196,7 +196,7 @@ def main(_):
 		train_x = np.random.randn(1)*10
 		train_y = slope * train_x  + intercept + np.random.randn(1) * 0.33
 
-		history, loss_v, step = sess.run([train_op, loss_value, global_step], feed_dict={inputv:train_x, label:train_y})
+		_, loss_v, step = sess.run([train_op, loss_value, global_step], feed_dict={inputv:train_x, label:train_y})
 	
 		if ischief and (step % steps_to_validate == 0):
 		  w,b, summary = sess.run([weight,bias,summary_op])
